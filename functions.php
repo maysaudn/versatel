4b64690906c8d6b506fc15874ea0d9335a0e2678
Nebal Maysaud <nmaysaud@gmail.com>
2026-03-20 16:42:01 -0400

    changed page header and CCS to simplified centered heading and single-color background

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@
 function versatel_files() {
     wp_enqueue_script('main-versatel-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true); // array can be null if not using jquery
     wp_enqueue_style('versatel_main_styles', get_theme_file_uri('/build/style-index.css'));
-    wp_enqueue_style('versatel_page_styles', get_theme_file_uri('/build/page-styles.css'));
-   // wp_enqueue_style('versatel_extra_styles', get_theme_file_uri('/build/extra-styles.css'));
+    // wp_enqueue_style('versatel_page_styles', get_theme_file_uri('/build/page-styles.css'));
+    // wp_enqueue_style('versatel_extra_styles', get_theme_file_uri('/build/extra-styles.css'));
     wp_enqueue_style('versatel_browser_styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_style('font-awesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css');
     wp_enqueue_style('font-awesome-stylesheet', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
